184208798c04c100a1eea819932b926cfb0b7233
kimi <sarahkmcaleer@gmail.com>
2021-08-21 16:33:51 -0400

    added Background component from playbook ui gave me padding I wanted for page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,16 +3,19 @@ import CardDashboard from '../CardDashboard/CardDashboard';
 import BlockNav from '../BlockNav/BlockNav';
 import LineGraphDefault from '../LineGraphDefault/LineGraphDefault';
 import KpiChart from '../KpiChart/KpiChart';
-
+import { Background } from 'playbook-ui'
 
 function App() {
   return (
     <div className="App">
+      <Background  backgroundColor="light"
+      padding="xl"
+      >
 <KpiChart/>
       <LineGraphDefault/>
       <BlockNav/>
       <CardDashboard/>
-
+      </Background>
     </div>
   );
 }
