9431376a85c87a4f302d454cf5898d027ece2801
kimi <sarahkmcaleer@gmail.com>
2021-08-21 17:11:16 -0400

    Using Card.Body not Card.Header for heading of Line Graph; using styled-component for heading style

--- src/components/KpiChart/KpiChart.js
+++ src/components/KpiChart/KpiChart.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import LineGraphNav from '../LineGraphNav/LineGraphNav';
 import LineGraphDefault from '../LineGraphDefault/LineGraphDefault';
-import { Card,Flex, FlexItem } from 'playbook-ui';
+import { Card,Flex, FlexItem, SectionSeparator, TitleDetail } from 'playbook-ui';
+import styled from 'styled-components'
 
-const KpiChart = () => (
+const GraphTitle = styled.p `
+color:#242b42;
+font-weight:bold;
+padding:10px 20px;
+text-align:left;
+`;
+
+const KpiChart = (props) => (
+
   <div>
     <Card padding="none">
+      {/* <Card.Header padding="xs" style={{BackgroundColor:"#fff"}}>
+        <GraphTitle>Key Performance Indicators</GraphTitle>
+
+    </Card.Header> */}
+     <Card.Body padding="none">
+     <GraphTitle>Key Performance Indicators</GraphTitle>
+      </Card.Body>
+      <SectionSeparator variant="card"></SectionSeparator>
      <Flex
         // className="flex-container"
         // horizontal="center"
