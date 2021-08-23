19afec7f53e14ffd07af01de7014990fdf72dc41
kimi <sarahkmcaleer@gmail.com>
2021-08-23 09:06:00 -0400

    creating top components for nav

---
+++ src/components/RightHeadNav/RightHeadNav.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+const RightHeadNav = () => (
+  <div>
+    RightHeadNav Component
+  </div>
+);
+
+RightHeadNav.propTypes = {};
+
+RightHeadNav.defaultProps = {};
+
+export default RightHeadNav;
