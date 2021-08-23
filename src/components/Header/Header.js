82b35d9c50eb70360541e15fbbc6ad1937556fbe
kimi <sarahkmcaleer@gmail.com>
2021-08-23 09:06:00 -0400

    creating top components for nav

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import LeftHeaderNav from '../LeftHeaderNav/LeftHeaderNav'
+import RightHeaderNav from '../RightHeadNav/RightHeadNav'
+
+
+const Header = () => (
+  <div>
+    <LeftHeaderNav></LeftHeaderNav>
+    <RightHeaderNav></RightHeaderNav>
+  </div>
+);
+
+export default Header;
