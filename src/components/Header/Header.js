a5ae0a924ec3febee6148ecd50f02850f3bcd436
kimi <sarahkmcaleer@gmail.com>
2021-08-26 13:35:25 -0400

    Revert "Merge branch 'Header-FixedFiles'"
    This reverts commit 17350607af59ca00cca7496c360ed329845e3544, reversing
    changes made to c3c82c7515c9cde6891378080a132c38db530e5c.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 import LeftHeaderNav from "../LeftHeaderNav/LeftHeaderNav";
 import RightHeaderNav from "../RightHeaderNav/RightHeaderNav";
-import {  Flex, FlexItem } from "playbook-ui";
+import {  Flex, FlexItem, Icon, Layout, Pill, Nav, NavItem, Title, User} from "playbook-ui";
 import logo from "../../images/logo.png"
 
 const Header = (props) => {
