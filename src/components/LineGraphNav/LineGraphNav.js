1d1a64172af0e93f100869fa15a6bda1f3892eda
kimi <sarahkmcaleer@gmail.com>
2021-08-26 14:20:59 -0400

    hardcoded json to pass to Line Graph Nav component with actual values from mockup

--- src/components/LineGraphNav/LineGraphNav.js
+++ src/components/LineGraphNav/LineGraphNav.js
@@ -12,18 +12,30 @@ import {
   Title,
 } from "playbook-ui";
 
+const lineGraphNavJSON = {
+  title: "Pipeline Chart",
+  navData: [
+    { label: "Revenue", percent: 26, change: "increase" },
+    { label: "Orders", percent: 2, change: "decrease" },
+    { label: "Profit", percent: "", change: "" },
+    { label: "Average Check", percent: 5, change: "increase" },
+    { label: "Canceled", percent: 18, change: "decrease" },
+    { label: "Repeat Sales", percent: "", change: "" },
+  ],
+};
 
-const DyanmicList = ({ index, items, selected, onRemove, propThatHasFunction }) => {
+
+const DyanmicList = ({ navData, selected, propThatHasFunction }) => {
 
   return (
     <>
 <Flex>
    <Nav orientation="vertical">
-   {items.map((item, index) => (
+   {navData.map((item, index) => (
      <NavItem className={selected === index ? "sideBorder" : ""} onClick={()=> propThatHasFunction(index)}>
       <Flex spacing="between">
-            <Title size={4} text="Revenue" />
-            <StatChange change="decrease" value="2" />
+            <Title size={4} text={item.label} />
+            <StatChange change={item.change} value={item.percent} />
           </Flex>
       </NavItem>
    ))}
@@ -48,6 +60,7 @@ return (
   items={[1,2,3]}
   selected={selected}
   propThatHasFunction={handleActiveItem}
+  {...lineGraphNavJSON}
 />
 </div>
   // <Flex>{selected2}
