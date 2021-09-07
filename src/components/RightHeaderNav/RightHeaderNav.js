6883d857012183b552908d6df422802a81d3d595
kimi <sarahkmcaleer@gmail.com>
2021-09-07 15:34:21 -0400

    Removed unused playbook components imports

--- src/components/RightHeaderNav/RightHeaderNav.js
+++ src/components/RightHeaderNav/RightHeaderNav.js
@@ -1,93 +1,57 @@
-import React, { useState } from 'react';
-import { Badge, Card, Collapsible, Flex, FlexItem, Icon, Nav, NavItem, User } from "playbook-ui"
-
+import React, { useState } from "react";
+import { Badge, Card, Icon, Nav, NavItem, User } from "playbook-ui";
 
 const RightHeaderNav = (props) => {
-const[inHover, setHover] = useState(false);
+  const [inHover, setHover] = useState(false);
 
-
-return (
-  <div>
-
- <Nav orientation="horizontal" padding="xs"  highlight={false}
-        variant="subtle">
-<NavItem>
-  <Icon
-          fixedWidth
-          icon="bell"
-          {...props}
-      />
-
-<Badge
-
-          text="4"
-          variant="primary"
-          {...props}
-      />
-</NavItem>
-<NavItem>
-<Icon
-          fixedWidth
-          icon="inbox"
-          {...props}
-      />
-       <Badge
-          text="2"
-          variant="primary"
-          {...props}
-      />
-</NavItem>
-<NavItem>
- <div
- onMouseEnter={() => setHover(true)}
- onMouseLeave={() => setHover(true)}
+  return (
+    <div>
+      <Nav
+        orientation="horizontal"
+        padding="xs"
+        highlight={false}
+        variant="subtle"
       >
+        <NavItem>
+          <Icon fixedWidth icon="bell" {...props} />
 
-      <User
-            align="left"
-            avatarUrl="https://randomuser.me/api/portraits/women/44.jpg"
-            name="Anna Black"
-            orientation="horizontal"
-            size="sm"
-            {...props}
-        />
-
-        </div>
-
-
-
+          <Badge text="4" variant="primary" {...props} />
         </NavItem>
-  </Nav>
+        <NavItem>
+          <Icon fixedWidth icon="inbox" {...props} />
+          <Badge text="2" variant="primary" {...props} />
+        </NavItem>
+        <NavItem>
+          <div
+            onMouseEnter={() => setHover(true)}
+            onMouseLeave={() => setHover(true)}
+          >
+            <User
+              align="left"
+              avatarUrl="https://randomuser.me/api/portraits/women/44.jpg"
+              name="Anna Black"
+              orientation="horizontal"
+              size="sm"
+              {...props}
+            />
+          </div>
+        </NavItem>
+      </Nav>
 
-
-{inHover &&
-<Card padding="none" className="hoverNav"
-onMouseLeave={() => setHover(false)}
->
-  <Nav
-
-      padding="none"
-      link="#"
-      orientation="vertical"
-      {...props}
-  >
-    <NavItem
-        link="#"
-        text="Profile"
-        {...props}
-    />
-    <NavItem
-        link="#"
-        text="Settings"
-        {...props}
-    />
-  </Nav>
-  </Card>
-
-}
-
-  </div>
-);
-}
+      {inHover && (
+        <Card
+          padding="none"
+          className="hoverNav"
+          onMouseLeave={() => setHover(false)}
+        >
+          <Nav padding="none" link="#" orientation="vertical" {...props}>
+            <NavItem link="#" text="Profile" {...props} />
+            <NavItem link="#" text="Settings" {...props} />
+          </Nav>
+        </Card>
+      )}
+    </div>
+  );
+};
 
 export default RightHeaderNav;
