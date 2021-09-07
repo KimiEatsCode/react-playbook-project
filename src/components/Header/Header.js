3643199a828b6aa03a0f8b1a3c1d1f9540f726ff
kimi <sarahkmcaleer@gmail.com>
2021-09-07 15:33:43 -0400

    Removed unused imports from playbook

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,28 +1,31 @@
 import React from "react";
 import LeftHeaderNav from "../LeftHeaderNav/LeftHeaderNav";
 import RightHeaderNav from "../RightHeaderNav/RightHeaderNav";
-import {  Flex, FlexItem, Icon, Layout, Pill, Nav, NavItem, Title, User} from "playbook-ui";
-import logo from "../../images/logo.png"
+import {
+  Flex,
+  FlexItem,
+} from "playbook-ui";
+import logo from "../../images/logo.png";
 
 const Header = (props) => {
+  return (
+    <>
+      <Flex orientation="row" justify="right" spacing="between" align="start">
+        <FlexItem>
+          <img className="logoTopNav" alt="PBJ logo" src={logo}></img>
+        </FlexItem>
 
+        <FlexItem>
+          <RightHeaderNav></RightHeaderNav>
+        </FlexItem>
+      </Flex>
 
-return (
-  <>
-   <Flex orientation="row" justify="right" spacing="between" align="start">
-     <FlexItem><img className="logoTopNav"alt="PBJ logo"src={logo}></img>
-     </FlexItem>
-
-     <FlexItem>
-     <RightHeaderNav></RightHeaderNav>
-     </FlexItem>
-     </Flex>
-     <Flex orientation="row">
-      <FlexItem>
-     <LeftHeaderNav></LeftHeaderNav>
-     </FlexItem>
-    </Flex>
-  </>
-);
-}
+      <Flex orientation="row">
+        <FlexItem>
+          <LeftHeaderNav></LeftHeaderNav>
+        </FlexItem>
+      </Flex>
+    </>
+  );
+};
 export default Header;
